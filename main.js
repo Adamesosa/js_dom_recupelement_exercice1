4c569ba795b2c31db014870e912e426cf261aa73
Adame <Adame.haouari@gmail.com>
2021-01-26 11:29:56 +0100

    fin exo

---
+++ main.js
@@ -0,0 +1,24 @@
+let monTitre = document.getElementsByTagName('h1')
+console.log(monTitre);
+
+
+let sousTitre = document.getElementsByTagName('h3')
+console.log(sousTitre);
+let firstSoustitre = sousTitre[0]
+let secondSoustitre = sousTitre[1]
+let threeSoustitre = sousTitre[2]
+
+console.log(threeSoustitre);
+
+
+let para = document.getElementsByTagName('p')
+console.log(para);
+let secondPara = para[1]
+console.log(secondPara);
+
+
+let allLi = document.getElementsByTagName('li')
+console.log(allLi);
+
+let threeLi = allLi[2]
+console.log(threeLi);
